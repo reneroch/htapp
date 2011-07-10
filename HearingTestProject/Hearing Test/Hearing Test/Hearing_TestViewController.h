17e88d1b0b5bfdede91fd1c6f6d6b70e5a575ca6
Far Rasmussen <computer@yonnrene.com>
2011-07-10 09:35:56 -0400

    Added Xcode project.

---
+++ HearingTestProject/Hearing Test/Hearing Test/Hearing_TestViewController.h
@@ -0,0 +1,15 @@
+//
+//  Hearing_TestViewController.h
+//  Hearing Test
+//
+//  Created by Dan Rene Rasmussen on 7/10/11.
+//  Copyright 2011 Qi Analytics LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Hearing_TestViewController : UIViewController {
+    
+}
+
+@end
---
+++ HearingTestProject/Hearing Test/Hearing Test/Hearing_TestViewController.m
@@ -0,0 +1,49 @@
+//
+//  Hearing_TestViewController.m
+//  Hearing Test
+//
+//  Created by Dan Rene Rasmussen on 7/10/11.
+//  Copyright 2011 Qi Analytics LLC. All rights reserved.
+//
+
+#import "Hearing_TestViewController.h"
+
+@implementation Hearing_TestViewController
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
